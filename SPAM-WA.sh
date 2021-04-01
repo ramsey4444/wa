a134d456e4ecd0690a4efb723ae993e0b77d79ea
ramsey4444 <81561237+ramsey4444@users.noreply.github.com>
2021-04-02 01:38:14 +0700

    Add files via upload

---
+++ SPAM-WA.sh
@@ -0,0 +1,32 @@
+#kode warna #MAU APA LO , MAU RECODE IZIN DULU TOLOL
+MERAH="\033[31;1m"
+HIJAU="\033[32;1m"
+echo -e $MERAH
+clear
+                          echo "_______________________"
+                          echo "AUTHOR : TERMUX-INDO NES"
+                          echo "YOUTUBE : TERMIX-INDO NES"
+                          echo "INSTAGRAM : TERMUX V2"
+                          echo "________________________"
+figlet SPam Wa.. | lolcat
+
+echo -e $HIJAU
+echo "AWALAN 08xxxxx"
+echo "________________"
+read -p "Nomor Target : " Nomor
+read -p "Jumlah Spam : " Spam
+echo "Mengirim ke  $Nomor "
+echo "Jumlah Spam  $Spam "
+echo "_____________________"
+echo "Succesfully [✓]"
+sleep 3
+echo "Succesfully [✓]"
+sleep 3
+echo "Succesfully [✓]"
+sleep 3
+echo "Succesfully [✓]"
+sleep 3
+echo "Succesfully [✓]"
+echo "------------------------"
+echo "Done Subscribe Heula"
+exit
